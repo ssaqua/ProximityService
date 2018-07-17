7b9a0a857cbbf1532091089a915e2e17d0234fb2
ssaqua <ssaqua@users.noreply.github.com>
2018-07-17 19:41:28 +1200

    Add API requirement annotation

--- app/src/main/kotlin/ss/proximityservice/ToggleTileService.kt
+++ app/src/main/kotlin/ss/proximityservice/ToggleTileService.kt
@@ -4,14 +4,17 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.os.Build
 import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
+import android.support.annotation.RequiresApi
 import android.support.v4.content.LocalBroadcastManager
 import ss.proximityservice.ProximityService.Companion.INTENT_NOTIFY_ACTIVE
 import ss.proximityservice.ProximityService.Companion.INTENT_NOTIFY_INACTIVE
 import ss.proximityservice.ProximityService.Companion.INTENT_START_ACTION
 import ss.proximityservice.ProximityService.Companion.INTENT_STOP_ACTION
 
+@RequiresApi(Build.VERSION_CODES.N)
 class ToggleTileService : TileService() {
     private val stateReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
